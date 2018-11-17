6ddd4ef225647bb40176c69ab6661b055a39027b
fanyinjiang <1757510543@qq.com>
2018-11-17 16:48:06 +0800

    change log.sh

--- vhost/log.sh
+++ vhost/log.sh
@@ -1,6 +1,9 @@
-#/bin/bash
-LOGPATH = /sakuraus/vhost/access.log
-RESULT = /sakuraus/backup_log/$(date +%Y%m%d)/access.log
-mkdir $RESULT
-mv $LOGPATH $RESULT
-
+#!/bin/bash
+LOGPATH=/sakuraus/vhost/access.log
+BASEPATH=/sakuraus/vhost/back_log #$(date -d yesterday +%Y%m)
+#echo $BASEPATH                                                             //echo 就是输出
+mkdir -p $BASEPATH
+bak=$BASEPATH/$(date -d yesterday +%Y%m%d%H%M).sakuraus.access.log
+mv $LOGPATH $bak
+touch $LOGPATH
+kill -USR1 `cat /run/nginx.pid`
