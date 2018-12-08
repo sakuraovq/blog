b3b31423c35e8299298ceb2e1188f7bb812d27ce
fanyinjiang <1757510543@qq.com>
2018-12-08 18:26:53 +0800

    Site updated: 2018-12-08 18:26:52

--- go/src/learn/crawler/zhenai/parser/userProfile.go
+++ go/src/learn/crawler/zhenai/parser/userProfile.go
@@ -1,13 +1,27 @@
 package parser
 
-import "learn/crawler/engine"
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"learn/crawler/engine"
+	"learn/crawler/fetcher"
+	"strings"
+)
 
 func init() {
 
 }
-func UserProfile(contents []byte) engine.ParserResult {
+func UserProfile(c []byte) engine.ParserResult {
 
 	userResult := engine.ParserResult{}
-
-	return  userResult
+	contents, e := fetcher.Fetch("http://album.zhenai.com/u/109816882")
+	fmt.Println("content",string(contents),e)
+	document, e := goquery.NewDocumentFromReader(strings.NewReader(string(contents)))
+	if e != nil {
+		fmt.Println("goquery error", e,c)
+	}
+	document.Find(".pink-btns").Each(func(i int, selection *goquery.Selection) {
+		fmt.Println(selection.Text())
+	})
+	return userResult
 }
